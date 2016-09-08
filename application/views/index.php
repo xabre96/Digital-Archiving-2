f59326147aba80f7062f4d7195a0b40e6298bf71
ICT <ICT>
2016-09-08 17:31:33 +0800

    Libraries loaded

--- application/views/index.php
+++ application/views/index.php
@@ -4,9 +4,14 @@
 	<meta charset="utf-8">
 	<title>Document Archiving System</title>
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url().'assets/angular-material/angular-material.min.css'; ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url().'assets/angular-material/layouts/angular-material.layouts.min.css'; ?>">
 </head>
 <body>
-	<h1>It works!</h1>
+
+	<script src="<?php echo base_url().'assets/angular/angular.min.js'; ?>"></script>
+	<script src="<?php echo base_url().'assets/angular/animateJs.js'; ?>"></script>
+	<script src="<?php echo base_url().'assets/angular/aria.js'; ?>"></script>
+	<script src="<?php echo base_url().'assets/angular/messages.js'; ?>"></script>
 	<script src="<?php echo base_url().'assets/angular-material/angular-material.js'; ?>"></script>
 </body>
 </html>
