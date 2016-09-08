203411d770f93d5331c4a779894f089f428f3df4
ICT <ICT>
2016-09-08 16:08:13 +0800

    save

--- application/views/index.php
+++ application/views/index.php
@@ -3,9 +3,10 @@
 <head>
 	<meta charset="utf-8">
 	<title>Document Archiving System</title>
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url().'assets/angular-material/angular-material.min.js'; ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url().'assets/angular-material/angular-material.min.css'; ?>">
 </head>
 <body>
 	<h1>It works!</h1>
+	<script src="<?php echo base_url().'assets/angular-material/angular-material.js'; ?>"></script>
 </body>
 </html>
